47cd486406cd6ddbda40d85d655d551893e9267b
William B McAllister <blakemcallister77@gmail.com>
2018-11-17 08:08:47 -0500

    Updated

--- InsuranceApplication/Controllers/AdminController.cs
+++ InsuranceApplication/Controllers/AdminController.cs
@@ -29,26 +29,6 @@ namespace InsuranceApplication.Controllers
 				}
 				return View(customerInfoVms);
 			}
-
-
-			//using (var db = new InsuranceEntities())
-			//{
-			//	var customerInfos = db.CustomerInfoes.ToList();
-			//	var customerInfoVms = new List<CustomerInfoVm>();
-			//	foreach (var customerInfo in customerInfos)
-			//	{
-			//		var customerInfoVm = new CustomerInfoVm();
-			//		customerInfoVm.Id = customerInfo.Id;
-			//		customerInfoVm.FirstName = customerInfo.FirstName;
-			//		customerInfoVm.LastName = customerInfo.LastName;
-			//		customerInfoVm.EmailAddress = customerInfo.EmailAddress;
-			//		customerInfoVm.Quote = customerInfo.Quote.ToString();
-			//		customerInfoVms.Add(customerInfoVm);
-
-			//	}
-			//	return View(customerInfoVms);
-			//}
-
 		}
     }
 }
--- InsuranceApplication/Controllers/HomeController.cs
+++ InsuranceApplication/Controllers/HomeController.cs
@@ -15,19 +15,13 @@ namespace InsuranceApplication.Controllers
 			return View();
 		}
 
-		//public ActionResult TestVm()
-		//{
-		//	return View();
-		//}
-
-
 		private InsuranceEntities db = new InsuranceEntities();
 
 		[HttpPost]
 		public ActionResult CustomerInfo(string firstName, string lastName, string emailAddress, string dateOfBirth, int carYear,
 										   string carMake, string carModel, bool dui, string speedingTicket, bool fullCoverage)
 		{
-			//CHECK FOR EMPTY FORM
+			//Check for Blanks
 			if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(dateOfBirth.ToString()) ||
 				string.IsNullOrEmpty(carYear.ToString()) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel) || string.IsNullOrEmpty(speedingTicket.ToString()))
 			{
@@ -36,7 +30,6 @@ namespace InsuranceApplication.Controllers
 			}
 			else
 			{
-				//DB INSTANSIATION 
 				using (InsuranceEntities db = new InsuranceEntities())
 				{
 					var customerInfo = new CustomerInfo();
@@ -55,7 +48,6 @@ namespace InsuranceApplication.Controllers
 					customerInfo.FullCoverage = fullCoverage;
 
 
-					//LOGIC OPPERATIONS
 					DateTime grab = DateTime.Parse(dateOfBirth);
 					int year = grab.Year;
 					int age = DateTime.Now.Year - year;
@@ -63,7 +55,7 @@ namespace InsuranceApplication.Controllers
 					double quote;
 					quote = 50.00;
 
-					//AGE CHECK
+					//Age
 					if (age < 18)
 					{
 						quote = quote + 100.00;
@@ -73,14 +65,13 @@ namespace InsuranceApplication.Controllers
 						quote = quote + 25.00;
 					}
 
-
-					//CAR YEAR CHECK
+					//Car Yr
 					if (carYear == 1999 || carYear > 2015)
 					{
 						quote = quote + 25.00;
 					}
 
-					//PORCHE CHECK 
+					//Check Car Model for Porsche 
 					if (carMake == "porsche")
 					{
 						quote = quote + 25.00;
@@ -90,7 +81,7 @@ namespace InsuranceApplication.Controllers
 						}
 					}
 
-					//SPEEDING TICKET
+					//Check for Speeding Tickets	
 					int ticketCount = Convert.ToInt32(speedingTicket);
 					if (ticketCount < 0)
 					{
@@ -107,7 +98,7 @@ namespace InsuranceApplication.Controllers
 						quote = quote + (quote * .25);
 					}
 
-					//FULL COVERAGE
+					//Full Coverage Check
 					if (fullCoverage == true)
 					{
 						quote = quote + (quote * .50);
@@ -120,95 +111,10 @@ namespace InsuranceApplication.Controllers
 
 					int userId = customerInfo.Id;
 
-					//   return View(Quoter(userId));
-
-
-
 					return RedirectToAction("Quote", new { Id = userId });
 				}
 			}
 		}
-
-
-		//public ActionResult Quoter(int Id)
-		//{
-		//    if (Id == null)
-		//    {
-		//        return View("~/Views/Shared/Error.cshtml");
-		//    }
-		//    CustomerTable customer = db.CustomerTables.Find(Id);
-		//    if (customer == null)
-		//    {
-		//        return View("~/Views/Shared/Error.cshtml");
-		//    }
-		//    return View(customer);
-		//}
-
-
-
-
-		//public ActionResult Quote(int? Id)
-		//{
-		//	using (InsuranceEntities db = new InsuranceEntities())
-		//	{
-		//		var returner = new QuoteVm();
-		//		returner.Id = returner.Id;
-		//		returner.FirstName = returner.FirstName;
-		//		returner.LastName = returner.LastName;
-		//		returner.Quote = returner.Quote;
-		//		return View(returner);
-		//	}
-
-
-		//}
-
-
-		//public ActionResult Quote(int? id)
-		//{
-
-		//    CustomerTable customer = db.CustomerTables.Find(id);
-
-		//    return View(customer);
-		//}
-
-
-		//[HttpPost]
-		//public ActionResult Quote(int Id)
-		//{
-		//    using (var db = new insuranceUhohEntities())
-		//    {
-		//        var finder = db.CustomerTables.Find(Id);
-		//        string quote = finder.Quote.ToString();
-		//        return View(quote);
-
-		//    }
-
-		//}
-
-
 	}
 }
 
-//{
-//	public class HomeController : Controller
-//	{
-//		public ActionResult Index()
-//		{
-//			return View();
-//		}
-
-//		public ActionResult About()
-//		{
-//			ViewBag.Message = "Your application description page.";
-
-//			return View();
-//		}
-
-//		public ActionResult Contact()
-//		{
-//			ViewBag.Message = "Your contact page.";
-
-//			return View();
-//		}
-//	}
-//}
